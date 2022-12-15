b0daec23b8178c3ba1a63e02969f94cfffa900c1
ThaliaLevy <thalialevy@hotmail.com>
2022-12-14 21:47:20 -0300

    Interpolação: dados dinâmicos

--- hora-de-codar/src/app/components/first-component/first-component.component.ts
+++ hora-de-codar/src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
-
+  nomeNoComponente: String = "Carlos";
+  cursoNoComponente: String = "Engenharia de software";
+  listaCursosNoComponente = ['Ciência da computação', 'Engenharia de software', 'Analise e desenvolvimento de sistemas', 'Engenharia da computação'];
+  objetoNoComponente = {
+    nome: "Fiat",
+    ano: "2009"
+  };
 }
