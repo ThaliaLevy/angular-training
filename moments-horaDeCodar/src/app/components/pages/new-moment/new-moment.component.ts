b0b894cb18628d808d04bf1052b8955f83e8ad64
ThaliaLevy <thalialevy@hotmail.com>
2022-12-18 23:01:32 -0300

    26: formulário com envio de foto

--- moments-horaDeCodar/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments-horaDeCodar/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-moment',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent {
-
+  btnText = "Compartilhar";
 }
