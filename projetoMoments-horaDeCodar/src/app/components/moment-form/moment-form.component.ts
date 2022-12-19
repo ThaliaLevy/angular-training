b908939026dce3d1732fcdb3126ad3482edea5d4
ThaliaLevy <thalialevy@hotmail.com>
2022-12-19 20:29:57 -0300

    28: validações no formulário

--- projetoMoments-horaDeCodar/src/app/components/moment-form/moment-form.component.ts
+++ projetoMoments-horaDeCodar/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,4 +8,33 @@ import { Component, Input } from '@angular/core';
 })
 export class MomentFormComponent {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  //usado para inicializar coisas do Angular (por isso não é pelo constructor)
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),  //garante que esse campo é obrigatório
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  //exclamação no final para indicar que esses valores podem não ser preenchidos
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log("enviou o formulário!");
+  }
 }
