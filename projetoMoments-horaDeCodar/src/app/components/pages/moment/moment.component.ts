9f1dbed21b95b448f241af9a8a698fbfdcd0c748
ThaliaLevy <thalialevy@hotmail.com>
2022-12-21 19:50:30 -0300

    36: layout > botão de excluir e editar

--- projetoMoments-horaDeCodar/src/app/components/pages/moment/moment.component.ts
+++ projetoMoments-horaDeCodar/src/app/components/pages/moment/moment.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-moment',
@@ -13,6 +15,10 @@ export class MomentComponent {
   constructor(private momentService: MomentService, private route: ActivatedRoute) { }
 
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
